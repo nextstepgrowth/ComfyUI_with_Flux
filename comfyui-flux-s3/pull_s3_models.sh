0c4dde48b74bedfdbdb2efc0dc5c75f76b7f5155
kyuwon lee <kyuwonlee@kyuwons-MacBook-Pro.local>
2024-12-10 02:41:05 +0900

    feat: volume 용량 변경

--- comfyui-flux-s3/pull_s3_models.sh
+++ comfyui-flux-s3/pull_s3_models.sh
@@ -4,8 +4,7 @@ if [ -z "$S3_BUCKET" ]; then
   echo "Error: S3_BUCKET environment variable is not set."
   exit 1
 fi
-aws configure set default.s3.max_concurrent_requests 50
+aws configure set default.s3.max_concurrent_requests 100
 
 echo "Pull models from: s3://$S3_BUCKET/models"
-
-aws s3 sync s3://$S3_BUCKET/models $MOUNT_MODELS --exact-timestamps
+aws s3 sync s3://$S3_BUCKET/models $MOUNT_MODELS --exact-timestamps  &> sync.log &
