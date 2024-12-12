5feba6187c0bcdf95e8004d081655a401b09466f
kyuuuw <duffufk1502@gmail.com>
2024-12-12 20:40:21 +0900

    fix: update S3_BUCKET variable to BUCKET_NAME and adjust fly.toml configuration

--- comfyui-flux-s3/pull_s3_models.sh
+++ comfyui-flux-s3/pull_s3_models.sh
@@ -1,10 +1,10 @@
 MOUNT_MODELS="/ComfyUI/models"
 
-if [ -z "$S3_BUCKET" ]; then
-  echo "Error: S3_BUCKET environment variable is not set."
+if [ -z "$BUCKET_NAME" ]; then
+  echo "Error: BUCKET_NAME environment variable is not set."
   exit 1
 fi
 aws configure set default.s3.max_concurrent_requests 100
 
-echo "Pull models from: s3://$S3_BUCKET/models"
-aws s3 sync s3://$S3_BUCKET/models $MOUNT_MODELS --exact-timestamps  --exclude "*/Florence/*"  &> sync_pull.log &
+echo "Pull models from: s3://$BUCKET_NAME/models"
+aws s3 sync s3://$BUCKET_NAME/models $MOUNT_MODELS --exact-timestamps  --exclude "*/Florence/*"  &> sync_pull.log &
